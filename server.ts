06c3250475c948b05773124e7b964ef797e292ef
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-02-27 16:00:35 +0400

    authentication successfully

--- server.ts
+++ server.ts
@@ -1,19 +1,25 @@
 import express from 'express';
 
 import orderRoutes from "./routes/order-routes";
-
+import authRoutes, {authenticateToken} from "./routes/authRoutes";
+import dotenv from "dotenv";
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
 
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
-    next();
-});
+dotenv.config();
+
+
+app.use(cors({
+    origin: 'http://localhost:8081',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use('/order',orderRoutes);
+app.use('/auth',authRoutes)
 
 
 app.listen(3005, (err=>{
@@ -23,3 +29,5 @@ app.listen(3005, (err=>{
 app.use('/',(req,res,next)=>{
     res.status(200).send('Not Found');
 })
+console.log("Loaded SECRET_KEY:", process.env.SECRET_KEY);
+app.use(authenticateToken);
